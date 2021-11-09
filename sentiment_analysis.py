6069a12e7da3a510a1ec6648c03ecaea8693204a
Tushar <kansaltushar18@gmail.com>
2021-11-09 01:32:03 -0500

    Flair Sent Anal

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,26 +3,42 @@ import numpy as np
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#!pip install flair
+import flair
+flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
 
 
 
-
-def func(split='training',path_to_dataset='./Data/TRAINING'):
-    data = pd.read_csv('./train_split.csv',sep="\t")
+def vader_sentiment(split='train',path_to_dataset='./Data/TRAINING'):
+    data = pd.read_csv(f'./{split}_split.csv',sep="\t")
     text = np.asarray(data.iloc[:, 6]).tolist()
     sid = SentimentIntensityAnalyzer()
     sentiments = {'pos':[],'neg':[],'neu':[]}
     for i in range(len(text)):
-    c = sid.polarity_scores(text[i])
-    key=0
-    v=0
-    ans=100000
-    for k in ['neg','neu','pos']:
-        if ans > c[k]:
-        key = k
-        v = c[k]
-        ans = c[k]
-    sentiments[key].append(i)
+        c = sid.polarity_scores(text[i])
+        key=0
+        v=0
+        ans=100000
+        for k in ['neg','neu','pos']:
+            if ans > c[k]:
+                key = k
+                v = c[k]
+                ans = c[k]
+        sentiments[key].append(i)
 
     return sentiments
+
+
+def flair(split='train',path_to_dataset='./Data/TRAINING'):
+    data = pd.read_csv(f'./{split}_split.csv',sep="\t")
+    text = np.asarray(data.iloc[:, 6]).tolist()
+    sentiments = {'POSITIVE':[],'NEGATIVE':[],'NEUTRAL':[]}
+    for i in range(len(text)):
+        s = flair.data.Sentence(text[i])
+        flair_sentiment.predict(s)
+        total_sentiment = s.labels
+        ans = str(total_sentiment[0]).split()
+        sentiments[ans[0]].append((i,ans[1]))
+    return sentiments
+
 
